4c2930f9703bcf42b3e87699a209c68d1ade070a
darkdimok <81861130+darkdimok@users.noreply.github.com>
2021-07-10 06:20:03 +0300

    Add files via upload

---
+++ Мырзаш Динмухамед день 4 группа 121б (2).cpp
@@ -0,0 +1,32 @@
+#include <SFML/Graphics.hpp>
+
+
+//Вариант 4
+//Изменяем горизонтальный размер прямоугольника
+
+int x = 50;
+int y = 50;
+bool t = false;
+
+int main() {
+	RenderWindow window(VideoMode(800, 600), "rectangle");
+	window.clear(Color::White);
+
+	RectangleShape rectangle(Vector2f(x , y));
+	rectangle.setFillColor(Color::Black);
+	rectangle.move(150, 150);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+
+
+			if (event.type == Event::KeyPressed)
+			{
+				if (event.key.code == Keyboard::D) t = true;
+				if (event.key.code == Keyboard::A) t = false;
+			}
